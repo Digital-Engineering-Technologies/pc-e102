468105eba818255c1b48151196a4f1a24e7d9c65
Oscar Jacobsen <Oscar.Jacobsen@norconsult.com>
2024-03-12 09:52:59 +0100

    123

---
+++ metadata.js
@@ -0,0 +1,51 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 17136031,
+    "boundingBox": {
+        "lx": 107063.906,
+        "ly": 1212610.381,
+        "lz": -4.014,
+        "ux": 107366.11500000004,
+        "uy": 1212912.59,
+        "uz": 298.19500000003168
+    },
+    "tightBoundingBox": {
+        "lx": 107063.906,
+        "ly": 1212610.381,
+        "lz": -4.014,
+        "ux": 107196.253,
+        "uy": 1212912.59,
+        "uz": 40.924
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 2.6172068119049074,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
